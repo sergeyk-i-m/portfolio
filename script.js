98d3e7a9272c4ab01edcc0a3b9cc5ca272abab58
Sergey Kim <97921237+sergeyk-i-m@users.noreply.github.com>
2024-11-25 01:54:19 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popover = new bootstrap.Popover('.popover-dismiss', {
+    trigger: 'focus'
+  })
